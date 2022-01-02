8f657657aa1fc1da48c0b4734ab95185053dbac0
bizbaeja <bjgbjg112@gmail.com>
2022-01-02 16:11:46 +0900

    0102 weather
    weather API

--- js/todo.js
+++ js/todo.js
@@ -12,12 +12,15 @@ function saveToDos() {
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter(toDos => toDos.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button")
     button.innerText = "❌";
     button.addEventListener("click",deleteToDo);
@@ -30,26 +33,25 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    const newTodoObj ={
-        text:newTodo,
+    const newTodoObj = {
+        text: newTodo,
         id: Date.now(),
     };
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
    }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-function sayHello(item) {
-    console.log("this is the turn of", item);
-}
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
 
-if(saveToDos !== null) { 
+if (savedToDos !== null) { 
     const parsedToDos = JSON.parse(savedToDos);
     toDos = parsedToDos;
     parsedToDos.forEach(paintToDo);
 }
+
+
